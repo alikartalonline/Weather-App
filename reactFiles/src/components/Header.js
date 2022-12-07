216a317424344c58a1b714db693f8314d6555fa4
Ali Kartal <ali_kartal91@hotmail.com>
2022-12-07 16:26:05 +0300

    first commit

---
+++ reactFiles/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+
+        <form action="">
+            <input type="text" />
+        </form>
+
+
+    </div>
+  )
+};
+
+export default Header;
---
+++ reactFiles/src/components/Homepage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+// COMPONENTS
+import Header from './Header';
+
+function Homepage() {
+    return (
+        <div>
+
+            <Header />
+
+
+        </div>
+    )
+};
+
+export default Homepage;
