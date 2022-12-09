d26837e2356a905d31f36a4b02393741b166aa9c
Ali Kartal <ali_kartal91@hotmail.com>
2022-12-09 22:18:00 +0300

    added

---
+++ src/components/MapDetail.js
@@ -0,0 +1,19 @@
+import React, { useMemo } from 'react';
+import { GoogleMap,Marker } from '@react-google-maps/api';
+
+function MapDetail({lat, lon}) {
+
+    const center = useMemo(() => ({ lat: lat, lng: lon }), [lat, lon]);
+    
+    return (
+        <GoogleMap
+            zoom={8} 
+            center={center}
+            mapContainerClassName="map-container">
+            <Marker position={center} />
+        </GoogleMap>
+    );
+}
+
+
+export default MapDetail;
