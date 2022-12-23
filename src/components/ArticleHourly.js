54d66e26fc6da24e2b48cd4d99e761f5da50e6f8
Ali Kartal <ali_kartal91@hotmail.com>
2022-12-23 20:19:29 +0300

    update

--- src/components/ArticleHourly.js
+++ src/components/ArticleHourly.js
@@ -8,6 +8,8 @@ function ArticleHourly({ lat, lon, item }) {
 
     const [hourly, setHourly] = useState([])
 
+    const options = { weekday: 'long' };
+    const options2 = { year: 'numeric', month: 'numeric', day: 'numeric' };
     // Hourly Weather Data (Call 5 day / 3 hour forecast data)
     useEffect(() => {
         axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}&units=metric`)
@@ -16,43 +18,12 @@ function ArticleHourly({ lat, lon, item }) {
             }).catch(err => {
                 console.log("hourly error", err);
             })
-    }, [item, lat, lon]);
 
-    // console.log("saatlik:", hourly.list[0].weather[0].main)
+    }, [lat, lon, item]);
 
-    const options = { weekday: 'long' };
+    // console.log("hourly:",hourly.city.name)
+    console.log("hourly item:",item)
 
-    // DAY 1: HOURS
-    const day1hour1 = new Date(hourly.list[0].dt_txt);
-    // const day1hour2 = new Date(hourly.list[1].dt_txt);
-    // const day1hour3 = new Date(hourly.list[2].dt_txt);
-    // const day1hour4 = new Date(hourly.list[3].dt_txt);
-    // const day1hour5 = new Date(hourly.list[4].dt_txt);
-    // const day1hour6 = new Date(hourly.list[5].dt_txt);
-    // const day1hour7 = new Date(hourly.list[6].dt_txt);
-    // const day1hour8 = new Date(hourly.list[7].dt_txt);
-
-    // DAYS
-    const day1weekDay = (day1hour1.toLocaleDateString('en-EN', options));
-    // const day2weekDay = (day2.toLocaleDateString('en-EN', options));
-    // const day3weekDay = (day3.toLocaleDateString('en-EN', options));
-    // const day4weekDay = (day4.toLocaleDateString('en-EN', options));
-    // const day5weekDay = (day5.toLocaleDateString('en-EN', options));
-
-
-
-    // const day2 = new Date(hourly.list[8].dt_txt);
-    // const day3 = new Date(hourly.list[16].dt_txt);
-    // const day4 = new Date(hourly.list[24].dt_txt);
-    // const day5 = new Date(hourly.list[32].dt_txt);
-
-
-    // console.log("saat :",day1hour1.getUTCHours());
-    // console.log("saat :",day1hour2.getUTCHours());
-    // console.log("saat :",day1hour3.getUTCHours());
-    // console.log("HAFTANIN GÜNÜ:",day2weekDay)
-    // console.log("günlük saat:",day2weekDay)
-    // console.log("günlük saat:",day1.toLocaleTimeString('en-US'));
 
 
     return (
@@ -62,27 +33,80 @@ function ArticleHourly({ lat, lon, item }) {
 
 
                     {
-                        item !== "" ?
-
+                        // hourly.city.name !== undefined ?
+                        hourly != "" ?
+                        
                             <div className='day1 col-2'>
-                                <h1>{day1weekDay}</h1>
-                                <span className='fs-6'>{hourly.list[1].dt_txt} </span>
-                                <ul>
+
+                                <div className='article-titles'>
+                                <h1>
+                                    {
+                                        new Date(hourly.list[0].dt_txt).toLocaleDateString('en-EN', options)
+                                    }
+                                </h1>
+
+                                <span className='fs-6'>
+                                    {
+                                        new Date(hourly.list[0].dt_txt).toLocaleDateString('en-EN', options2)
+                                    } 
+                                </span>
+                                    </div>
+
+                                <ul className='mt-2'>
+
                                     <li>
-                                        {day1hour1.getUTCHours()} : 00 <span><img src={`http://openweathermap.org/img/wn/${hourly.list[0].weather[0].icon}.png`} alt="" /></span><span>{hourly.list[1].main.temp}°C</span>
+                                        {
+                                            new Date(hourly.list[0].dt_txt).getUTCHours() > 9 ? new Date(hourly.list[0].dt_txt).getUTCHours() : "0" + new Date(hourly.list[0].dt_txt).getUTCHours() 
+                                        } : 00 <span><img src={`http://openweathermap.org/img/wn/${hourly.list[0].weather[0].icon}.png`} alt="" /></span><span>{hourly.list[0].main.temp}°C</span>
                                     </li>
+
                                     <li>
-                                        {/* {hourly.list[1].dt_txt}  */}
+                                        {
+                                            new Date(hourly.list[1].dt_txt).getUTCHours() > 9 ? new Date(hourly.list[1].dt_txt).getUTCHours() : "0" + new Date(hourly.list[1].dt_txt).getUTCHours() 
+                                        } : 00 <span><img src={`http://openweathermap.org/img/wn/${hourly.list[1].weather[0].icon}.png`} alt="" /></span><span>{hourly.list[1].main.temp}°C</span>
                                     </li>
+
                                     <li>
-                                        {/* {hourly.list[5].dt_txt}  */}
+                                        {
+                                            new Date(hourly.list[2].dt_txt).getUTCHours() > 9 ? new Date(hourly.list[2].dt_txt).getUTCHours() : "0" + new Date(hourly.list[2].dt_txt).getUTCHours() 
+                                        } : 00 <span><img src={`http://openweathermap.org/img/wn/${hourly.list[2].weather[0].icon}.png`} alt="" /></span><span>{hourly.list[2].main.temp}°C</span>
+                                    </li>
+
+                                    <li>
+                                        {
+                                            new Date(hourly.list[3].dt_txt).getUTCHours() > 9 ? new Date(hourly.list[3].dt_txt).getUTCHours() : "0" + new Date(hourly.list[3].dt_txt).getUTCHours() 
+                                        } : 00 <span><img src={`http://openweathermap.org/img/wn/${hourly.list[3].weather[0].icon}.png`} alt="" /></span><span>{hourly.list[3].main.temp}°C</span>
+                                    </li>
+
+                                    <li>
+                                        {
+                                            new Date(hourly.list[4].dt_txt).getUTCHours() > 9 ? new Date(hourly.list[4].dt_txt).getUTCHours() : "0" + new Date(hourly.list[4].dt_txt).getUTCHours() 
+                                        } : 00 <span><img src={`http://openweathermap.org/img/wn/${hourly.list[4].weather[0].icon}.png`} alt="" /></span><span>{hourly.list[4].main.temp}°C</span>
+                                    </li>
+
+
+                                    <li>
+                                        {
+                                            new Date(hourly.list[5].dt_txt).getUTCHours() > 9 ? new Date(hourly.list[5].dt_txt).getUTCHours() : "0" + new Date(hourly.list[5].dt_txt).getUTCHours() 
+                                        } : 00 <span><img src={`http://openweathermap.org/img/wn/${hourly.list[5].weather[0].icon}.png`} alt="" /></span><span>{hourly.list[5].main.temp}°C</span>
+                                    </li>
+
+
+                                    <li>
+                                        {
+                                            new Date(hourly.list[6].dt_txt).getUTCHours() > 9 ? new Date(hourly.list[6].dt_txt).getUTCHours() : "0" + new Date(hourly.list[6].dt_txt).getUTCHours() 
+                                        } : 00 <span><img src={`http://openweathermap.org/img/wn/${hourly.list[6].weather[0].icon}.png`} alt="" /></span><span>{hourly.list[6].main.temp}°C</span>
+                                    </li>
+
+                                    <li>
+                                        {
+                                            new Date(hourly.list[7].dt_txt).getUTCHours() > 9 ? new Date(hourly.list[7].dt_txt).getUTCHours() : "0" + new Date(hourly.list[7].dt_txt).getUTCHours() 
+                                        } : 00 <span><img src={`http://openweathermap.org/img/wn/${hourly.list[7].weather[0].icon}.png`} alt="" /></span><span>{hourly.list[7].main.temp}°C</span>
                                     </li>
                                 </ul>
                             </div>
 
-
-
-                            : null
+                        : null
                     }
 
 
